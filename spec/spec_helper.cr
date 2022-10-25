d04197d4375e106104a1bc3257649940d505b7a6
MoriGM <30366747+MoriGM@users.noreply.github.com>
2022-10-25 14:35:42 +0200

    Fix tests import with loop

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/zinn"
+require "../src/terminal.cr"
--- spec/zinn_spec.cr
+++ spec/zinn_spec.cr
@@ -1,7 +1,7 @@
 require "./spec_helper"
 
-describe "Zinn" do
-  describe "#size" do
+describe Terminal do
+  describe "size" do
     it "correctly reports the number of elements in the Array" do
       [1, 2, 3].size.should eq 3
     end
